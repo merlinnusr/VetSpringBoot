d987ce0892950480d56bc6b455c00f5db1dee79a
Cocoa Guy <supercool4444@protonmail.com>
2024-10-15 19:56:29 -0600

    I added new validations

--- src/main/java/com/vet/backend/models/Adoption.java
+++ src/main/java/com/vet/backend/models/Adoption.java
@@ -3,6 +3,7 @@ package com.vet.backend.models;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.vet.backend.enums.AnimalType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -12,6 +13,8 @@ public class Adoption {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(max = 50, message = "Name cannot be longer than 50 characters")
+    @Column(length = 50)  // This sets the column length in the database
     private String name;
     @ManyToOne
     @JoinColumn(name = "pet_id", nullable = false)
--- src/main/java/com/vet/backend/models/Pet.java
+++ src/main/java/com/vet/backend/models/Pet.java
@@ -3,6 +3,7 @@ package com.vet.backend.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,6 +17,8 @@ public class Pet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(max = 50, message = "Name cannot be longer than 50 characters")
+    @Column(length = 50)  // This sets the column length in the database
     private String name;
     @ManyToOne
     @JoinColumn(name = "animal_type_id", nullable = false)
