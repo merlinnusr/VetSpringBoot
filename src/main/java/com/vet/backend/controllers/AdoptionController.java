9ce10ee3caae4ba0b3e6242da6315cb0637801d1
MAXII\maxii <maximaxmaxii690@gmail.com>
2024-11-04 13:00:27 -0300

    add CRUD adoption Controller

--- src/main/java/com/vet/backend/controllers/AdoptionController.java
+++ src/main/java/com/vet/backend/controllers/AdoptionController.java
@@ -3,18 +3,18 @@ package com.vet.backend.controllers;
 import com.vet.backend.dtos.AdoptionDto;
 import com.vet.backend.models.Adoption;
 import com.vet.backend.models.Pet;
+import com.vet.backend.services.IAdoptionService;
 import com.vet.backend.services.imp.AdoptionService;
 import com.vet.backend.services.imp.AnimalTypeService;
 import com.vet.backend.services.imp.PetService;
 import com.vet.backend.services.imp.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -24,7 +24,7 @@ public class AdoptionController {
     //@Autowired
     //private PetService petService;
     @Autowired
-    private AdoptionService adoptionService;
+    private IAdoptionService adoptionService;
     //@Autowired
     //private UserService userService;
     //@Autowired
@@ -49,4 +49,30 @@ public class AdoptionController {
 //        this.petService.store(pet);
         return this.adoptionService.store(adoptionDto);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Adoption> getAdoptionById(@PathVariable("id") Long id){
+        Adoption adoption = this.adoptionService.findAdoptionById(id);
+        return ResponseEntity.ok(adoption);
+    }
+
+    @GetMapping("/traer/todos")
+    public List<Adoption> getAllAdoptions(){
+        return this.adoptionService.getAdoptions();
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<?> deleteAdoptionById(@PathVariable("id") Long id){
+        this.adoptionService.deleteAdoption(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<?> editAdoption(@PathVariable("id") Long id,
+                                          @RequestBody AdoptionDto adoptionDto){
+        Adoption adoption = this.adoptionService.updateAdoption(id, adoptionDto);
+        return ResponseEntity.ok(adoption);
+    }
+
+
 }
