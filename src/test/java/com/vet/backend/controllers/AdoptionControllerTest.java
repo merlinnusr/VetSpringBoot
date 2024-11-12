049cc0236cdf2e19b8d4b1aef78cd08c4a6f8ade
MAXII\maxii <maximaxmaxii690@gmail.com>
2024-11-12 17:06:56 -0300

    add test adoption controller

--- src/test/java/com/vet/backend/controllers/AdoptionControllerTest.java
+++ src/test/java/com/vet/backend/controllers/AdoptionControllerTest.java
@@ -16,18 +16,24 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class AdoptionControllerTest {
     private MockMvc mockMvc;
 
@@ -99,4 +105,120 @@ public class AdoptionControllerTest {
 //                .andExpect(jsonPath("$.user.id", is(1)))
 //                .andExpect(jsonPath("$.pet.name", is("Firulais")));
     }
+
+    @Test
+    void getAdoptionByIdTest() throws Exception{
+        User user = new User();
+        user.setId(1L);
+
+        Pet pet = new Pet();
+        pet.setId(1L);
+        pet.setName("Firulais");
+
+        Adoption adoption = new Adoption();
+        adoption.setId(1L);
+        adoption.setName("Adopción 1");
+        adoption.setUser(user);
+        adoption.setPet(pet);
+        adoption.setAdoptionDate(LocalDate.now());
+
+        when(adoptionService.findAdoptionById(1L)).thenReturn(adoption);
+
+        mockMvc.perform(get("/api/adopciones/{id}", 1)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Adopción 1"))
+                .andExpect(jsonPath("$.user.id").value(1))
+                .andExpect(jsonPath("$.pet.name").value("Firulais"));
+    }
+
+    @Test
+    void getAllAdoptionsTest() throws Exception {
+        User user1 = new User();
+        user1.setId(1L);
+        User user2 = new User();
+        user2.setId(2L);
+
+        Pet pet1 = new Pet();
+        pet1.setId(1L);
+        pet1.setName("Firulais");
+
+        Pet pet2 = new Pet();
+        pet2.setId(2L);
+        pet2.setName("Lassie");
+
+        Adoption adoption1 = new Adoption();
+        adoption1.setId(1L);
+        adoption1.setName("Adopción 1");
+        adoption1.setUser(user1);
+        adoption1.setPet(pet1);
+        adoption1.setAdoptionDate(LocalDate.now());
+
+        Adoption adoption2 = new Adoption();
+        adoption2.setId(2L);
+        adoption2.setName("Adopción 2");
+        adoption2.setUser(user2);
+        adoption2.setPet(pet2);
+        adoption2.setAdoptionDate(LocalDate.now());
+
+        List<Adoption> adoptions = Arrays.asList(adoption1, adoption2);
+
+        when(adoptionService.getAdoptions()).thenReturn(adoptions);
+
+        mockMvc.perform(get("/api/adopciones/traer/todos")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].name").value("Adopción 1"))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].name").value("Adopción 2"));
+    }
+
+    @Test
+    void deleteAdoptionByIdTest() throws Exception {
+        mockMvc.perform(delete("/api/adopciones/eliminar/{id}", 1)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void editAdoptionTest() throws Exception {
+        AdoptionDto adoptionDto = new AdoptionDto();
+        adoptionDto.setUserId(1L);
+        adoptionDto.setName("Adopción Editada");
+        adoptionDto.setPetId(2L);
+
+        User user = new User();
+        user.setId(1L);
+
+        Pet pet = new Pet();
+        pet.setId(2L);
+        pet.setName("Lassie");
+
+        Adoption adoption = new Adoption();
+        adoption.setId(1L);
+        adoption.setName("Adopción Editada");
+        adoption.setUser(user);
+        adoption.setPet(pet);
+        adoption.setAdoptionDate(LocalDate.now());
+
+        when(adoptionService.updateAdoption(1L, adoptionDto)).thenReturn(adoption);
+
+        mockMvc.perform(put("/api/adopciones/editar/{id}", 1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "userId": 1,
+                                    "petId": 2,
+                                    "name": "Adopción Editada"
+                                }
+                                """))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Adopción Editada"))
+                .andExpect(jsonPath("$.pet.name").value("Lassie"))
+                .andExpect(jsonPath("$.user.id").value(1));
+    }
+
 }
